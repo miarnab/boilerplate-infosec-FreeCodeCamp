a4dc43b17187587ed10efdff282a4b15207f6308
Arnab Mitra <mitraarnab.2013@gmail.com>
2021-05-31 10:16:41 +0530

    Initial Commit

---
+++ myApp.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var helmet = require('helmet');
+var app = express();
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({
+  action:'deny'
+})
+);
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+const ninetydaysinseconds=90*24*60*60;
+var timeinseconds= ninetydaysinseconds;
+app.use(helmet.hsts({
+  maxAge: timeinseconds,
+  force: true
+}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({ directives:{
+  defaultSrc: ["'self'"],
+  scriptSrc:["'self'",'trusted-cdn.com']
+}}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+var api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
